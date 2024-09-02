3b4e8d16b733fee2262be62b1f2f0b7d318e5feb
INDavid04 <davidirimia04@gmail.com>
2024-09-02 21:03:42 +0300

    add: about page with header navigation

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Parallax
+
 let sun = document.getElementById('sun');
 let mountains = document.getElementById('mountains');
 let clouds = document.getElementById('clouds');
@@ -14,28 +16,4 @@ window.addEventListener('scroll', function() {
     clouds.style.right = 40 - value*0.1 + 'vw';
     clouds.style.bottom = 120 - value*0.15 + 'vw';
     hills.style.bottom = 7 - value*0.12 + 'vw';
-})
-
-let textElement = document.querySelector('.text');
-let textContent = textElement.textContent;
-textElement.innerHTML = '';
-
-for (let char of textContent) {
-    let span = document.createElement('span');
-    span.textContent = char;
-    textElement.appendChild(span);
-}
-
-let spans = textElement.querySelectorAll('span');
-// add scroll event listener; window.innerWidth < 480
-window.addEventListener('scroll', () => {
-    console.log('scroll');
-    let scrollDistance = window.scrollY;
-    spans.forEach((span, index) => {
-        if (scrollDistance >= (index + 1) * 1.5) {
-            span.classList.add('active');
-        } else {
-            span.classList.remove('active');
-        }
-    })
 })
